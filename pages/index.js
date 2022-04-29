be3da335d38775d657d298bbc5589fa3b160eb7b
unknown <setprogramming@gmail.com>
2022-04-29 16:53:15 +0300

    added-the-footer

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import {SearchIcon, MicrophoneIcon} from '@heroicons/react/solid'
+import Footer from '../components/Footer'
 
 
 export default function Home() {
@@ -24,7 +25,7 @@ export default function Home() {
          <div className="flex w-full mt-5 justify-center max-w-[90%] border border-gray-200 hover:shadow-md focus-within:shadow-md px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
             <SearchIcon className="h-5 text-gray-500 mr-3" />
             <input type="text" className="flex-grow focus:outline-none" />
-            <MicrophoneIcon className="h-5" />
+            <MicrophoneIcon className="h-5 text-gray-500" />
          </div>
          <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
             <button className="btn">Google Search</button>
@@ -34,6 +35,7 @@ export default function Home() {
       </form>
 
       {/* Footer */}
+      <Footer />
       
     </div>
   )
